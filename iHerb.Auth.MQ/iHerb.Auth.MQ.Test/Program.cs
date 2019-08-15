bf1aee081f874499dcc22a90ac2a5e6fd1a8e3d6
hong.li <hong.li@iherb.com>
2019-08-15 15:50:37 +0800

    MQ

---
+++ iHerb.Auth.MQ/iHerb.Auth.MQ.Test/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Polly;
+using System;
+using System.IO;
+
+namespace iHerb.Auth.MQ.Test
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var config = LoadConfig();
+            var msg = new mytest();
+            msg.Num = 123;
+            var maoper=MQProvider.CreateInstance(config);
+            //maoper.SendMessage("lh", msg);
+            maoper.Listen<mytest>("lh", GetMsg);
+
+        }
+
+        static IConfigurationRoot LoadConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+            return config;
+        }
+
+        public class mytest
+        {
+            public int Num { get; set; } = 0;
+        }
+
+        public static bool GetMsg(mytest ms)
+        {
+            Console.WriteLine("Hello World2!");
+            return true;
+        }
+    }
+}
---
+++ iHerb.Auth.MQ/iHerb.Auth.MQ/Interface/IConsume .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iHerb.Auth.MQ.Interface
+{
+    public interface IConsume
+    {
+        void Listen<T>(string qName, Func<T, bool> allwaysRunAction, SerializeType tyep = SerializeType.Json) where T : class;
+
+
+        void Listen<T>(string exchangeName, Func<T, bool> allwaysRunAction, string exchangeType, string routingKey = "", SerializeType type = SerializeType.Json) where T : class;
+
+    }
+}
